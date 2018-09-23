0a6944f97064805fd6b11ec3bf5b0a824ecf4044
josekang <josekangethe2@gmail.com>
2018-09-23 23:27:47 +0300

    editted files

--- Test/test_app.py
+++ Test/test_app.py
@@ -1,9 +1,7 @@
 from copy import deepcopy
 import unittest
 import json
-
 from app.views import app,orders
-
 
 BASE_URL = 'http://127.0.0.1:5000/api/v1/orders'
 BAD_ORDER_URL = '{}/5'.format(BASE_URL)
@@ -19,42 +17,80 @@ class TestFlaskApi(unittest.TestCase):
     def test_get_all(self):
         response = self.app.get(BASE_URL)
         data = json.loads(response.get_data())
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data['orders']), 2)
+        self.assertEqual(response.status_code, 404)
 
     def test_get_one(self):
-        response = self.app.get(BASE_URL)
+        response = self.app.get(BASE_URL+'/3')
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 404)
+    
+    def test_post_one(self):
+        data = {
+        'id': '1',
+        'name': 'the name',
+        'value':'33432'
+        }
+        response=self.app.post(BASE_URL, 
+                       data=json.dumps(data),
+                       content_type='application/json')
+        anyname = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_one_two(self):
+        data = {
+        'id': '1',
+        'name': 'the name',
+        'value':'33432'
+        }
+        first_response=self.app.post(BASE_URL, 
+                       data=json.dumps(data),
+                       content_type='application/json')
+        response = self.app.get(BASE_URL+'/1')
         data = json.loads(response.get_data())
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['orders'][0]['name'], 'refreshments')
 
+    def test_not_get_one(self):
+        data = {
+        'id': '1',
+        'name': 'the name',
+        'value':'33432'
+        }
+        first_response=self.app.post(BASE_URL, 
+                       data=json.dumps(data),
+                       content_type='application/json')
+        response = self.app.get(BASE_URL+'/2')
+        data = json.loads(response.get_data())
+        self.assertEqual(response.status_code, 404)
+     
     def test_order_not_exist(self):
         response = self.app.get(BAD_ORDER_URL)
         self.assertEqual(response.status_code, 404)
 
-    def test_update(self):
-        order = {"status": 1}
-        response = self.app.put(BASE_URL+'/1',
-                                data=json.dumps(order),
-                                content_type='application/json')
-        self.assertEqual(response.status_code, 201)
-        data = json.loads(response.get_data())
-        self.assertEqual(data['order']['status'], 1)
-       
+    def test_update(self): 
+        client = app.test_client(self)
+        self.new_orders={"id": 1, "status": 0}
+        self.old_edit= {"id": 1, "status":1  }
+        data = client.put(BASE_URL+'/1',
+                                 data = json.dumps(self.new_orders), 
+                                 content_type="application/json")
+        order = client.get(BASE_URL+'/1',
+                             content_type="application/json")
+        self.assertEqual(order.status_code, 200)
+           
     def test_update_error(self):
         # cannot edit non-existing item
         order = {"value": 30}
         response = self.app.put(BAD_ORDER_URL,
                                 data=json.dumps(order),
                                 content_type='application/json')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 405)
 
     def test_delete(self):
-        response = self.app.delete(BASE_URL+'/0',
+        response = self.app.delete(BASE_URL + '/0',
                                 content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code,405)
         response = self.app.delete(BAD_ORDER_URL)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 405)
 
     def tearDown(self):
         # reset app.items to initial state
--- app/views.py
+++ app/views.py
@@ -5,26 +5,7 @@ NOT_FOUND = 'Not found'
 BAD_REQUEST = 'Bad request'
 
 app = Flask(__name__)
-orders = [
-    {
-        'id': 1,
-        'name': 'vegetables',
-        'value': 1000,
-        'status': 0
-    },
-    {
-        'id': 2,
-        'name': 'refreshments',
-        'value': 300,
-        'status': 0
-    },
-    {
-        'id': 3,
-        'name': 'fruits',
-        'value': 20,
-        'status': 0
-    },
-]
+orders = [] 
 
 def _get_order(id):
     return [order for order in orders if order['id'] == id]
@@ -36,7 +17,6 @@ def _record_exists(name):
 def not_found(error):
     return make_response(jsonify({'error': NOT_FOUND}), 404)
 
-
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': BAD_REQUEST}), 400)
@@ -45,19 +25,20 @@ def bad_request(error):
 def index():
     return render_template('index.html')
 
-
 @app.route('/api/v1/orders', methods=['GET'])
 def get_orders():
-    return jsonify({'orders': orders})
-
+    if orders:
+        return jsonify({'orders': orders}),200
+    else: abort(404)    
 
 @app.route('/api/v1/orders/<int:id>', methods=['GET'])
 def get_order(id):
-    order = _get_order(id)
-    if not order:
-        abort(404)
-    return jsonify({'orders': order})
-
+    if orders:
+        order = _get_order(id)
+        if not order:
+            abort(404)
+        return jsonify({'order': order}),200
+    else: abort(404)
 
 @app.route('/api/v1/orders', methods=['POST'])
 def create_order():
@@ -74,18 +55,19 @@ def create_order():
             type=int,
             required=True,
     )
-    parser.add_argument("status",type=int,
-            required=True)
+    parser.add_argument("status",
+            type=int)
     data = parser.parse_args()
-    order = {"id": data['id'], "name": data['name'],
-            "value": data['value'],"status": data['status']}
-    orders.append(order)
-    return jsonify({'order': order}), 201
+    orders.append(data)
+    return jsonify({'order': data}), 200
 
-
-@app.route('/api/v1/orders/<int:id>', methods=['PUT'])
-def update_order(id):
+@app.route('/api/v1/orders', methods=['PUT'])
+def update_order():
     parser = reqparse.RequestParser()
+    parser.add_argument("id",
+            type=int,
+            required=True,
+    )
     parser.add_argument("status",
             type=int,
             required=True,
@@ -93,19 +75,30 @@ def update_order(id):
     data = parser.parse_args()
     status = data['status']
     if type(status) is not int:
-        abort(400)
-    orders[id]['status'] = status
-    order = {"id":id, "name": orders[id]['name'],
-        "value":orders[id]['value'],"status": orders[id]['status']}
-    return jsonify({'order': order}), 201
+        abort(400)                         
+    for order in orders:
+        if order['id']==data['id']:
+            order['status']=data['status']
+            return jsonify({'order': order},200)
+        else: abort(404)    
 
-
-@app.route('/api/v1/orders/<int:id>', methods=['DELETE'])
-def delete_order(id):
-    if orders and id<len(orders):
-        order=orders[id]
-        del orders[id]
-        return jsonify({'order': order}), 201
+@app.route('/api/v1/orders', methods=['DELETE'])
+def delete_order():
+    parser = reqparse.RequestParser()
+    parser.add_argument("id",
+            type=int,
+            required=True,
+    )
+    data = parser.parse_args()
+    if orders:
+        for i,order in enumerate(orders):
+            if order['id']==data['id']:
+                del_order=order
+                del  orders[i]
+                return jsonify({'order': del_order},200)
+            else: abort(404)
     else:
-        return jsonify({'error': "no items left"}), 404
+        return jsonify({'error': 'No Items Left'},200)
 
+if __name__ == '__main__':
+    main()
